364e6668f6c2a7886f5115baa3f4955855c299eb
octantus <kksova@ya.ru>
2024-02-29 16:13:12 +0300

    added some new files

---
+++ Form1.Designer.vb
@@ -0,0 +1,288 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        exit_button = New Button()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
+        Button9 = New Button()
+        Button10 = New Button()
+        Button11 = New Button()
+        Button12 = New Button()
+        Button13 = New Button()
+        Button14 = New Button()
+        Button15 = New Button()
+        Button16 = New Button()
+        Button17 = New Button()
+        Button18 = New Button()
+        Button19 = New Button()
+        TextBox1 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        SuspendLayout()
+        ' 
+        ' exit_button
+        ' 
+        exit_button.BackColor = Color.IndianRed
+        resources.ApplyResources(exit_button, "exit_button")
+        exit_button.FlatAppearance.BorderColor = Color.IndianRed
+        exit_button.ForeColor = SystemColors.ControlLightLight
+        exit_button.Name = "exit_button"
+        exit_button.UseVisualStyleBackColor = False
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.SandyBrown
+        Button1.FlatAppearance.BorderColor = Color.SandyBrown
+        resources.ApplyResources(Button1, "Button1")
+        Button1.Name = "Button1"
+        Button1.UseVisualStyleBackColor = False
+        ' 
+        ' Button2
+        ' 
+        Button2.BackColor = Color.SandyBrown
+        Button2.FlatAppearance.BorderColor = Color.SandyBrown
+        resources.ApplyResources(Button2, "Button2")
+        Button2.Name = "Button2"
+        Button2.UseVisualStyleBackColor = False
+        ' 
+        ' Button3
+        ' 
+        Button3.BackColor = Color.SandyBrown
+        Button3.FlatAppearance.BorderColor = Color.SandyBrown
+        resources.ApplyResources(Button3, "Button3")
+        Button3.Name = "Button3"
+        Button3.UseVisualStyleBackColor = False
+        ' 
+        ' Button4
+        ' 
+        Button4.BackColor = Color.SandyBrown
+        Button4.FlatAppearance.BorderColor = Color.SandyBrown
+        resources.ApplyResources(Button4, "Button4")
+        Button4.Name = "Button4"
+        Button4.UseVisualStyleBackColor = False
+        ' 
+        ' Button5
+        ' 
+        Button5.BackColor = Color.LightGray
+        Button5.FlatAppearance.BorderColor = Color.LightGray
+        resources.ApplyResources(Button5, "Button5")
+        Button5.Name = "Button5"
+        Button5.UseVisualStyleBackColor = False
+        ' 
+        ' Button6
+        ' 
+        Button6.BackColor = Color.LightGray
+        Button6.FlatAppearance.BorderColor = Color.LightGray
+        resources.ApplyResources(Button6, "Button6")
+        Button6.Name = "Button6"
+        Button6.UseVisualStyleBackColor = False
+        ' 
+        ' Button7
+        ' 
+        Button7.BackColor = Color.LightGray
+        Button7.FlatAppearance.BorderColor = Color.LightGray
+        resources.ApplyResources(Button7, "Button7")
+        Button7.Name = "Button7"
+        Button7.UseVisualStyleBackColor = False
+        ' 
+        ' Button8
+        ' 
+        Button8.BackColor = Color.SandyBrown
+        Button8.FlatAppearance.BorderColor = Color.SandyBrown
+        resources.ApplyResources(Button8, "Button8")
+        Button8.Name = "Button8"
+        Button8.UseVisualStyleBackColor = False
+        ' 
+        ' Button9
+        ' 
+        Button9.BackColor = Color.LightGray
+        Button9.FlatAppearance.BorderColor = Color.LightGray
+        resources.ApplyResources(Button9, "Button9")
+        Button9.Name = "Button9"
+        Button9.UseVisualStyleBackColor = False
+        ' 
+        ' Button10
+        ' 
+        Button10.BackColor = Color.LightGray
+        Button10.FlatAppearance.BorderColor = Color.LightGray
+        resources.ApplyResources(Button10, "Button10")
+        Button10.Name = "Button10"
+        Button10.UseVisualStyleBackColor = False
+        ' 
+        ' Button11
+        ' 
+        Button11.BackColor = Color.LightGray
+        Button11.FlatAppearance.BorderColor = Color.LightGray
+        resources.ApplyResources(Button11, "Button11")
+        Button11.Name = "Button11"
+        Button11.UseVisualStyleBackColor = False
+        ' 
+        ' Button12
+        ' 
+        Button12.BackColor = Color.SandyBrown
+        Button12.FlatAppearance.BorderColor = Color.SandyBrown
+        resources.ApplyResources(Button12, "Button12")
+        Button12.Name = "Button12"
+        Button12.UseVisualStyleBackColor = False
+        ' 
+        ' Button13
+        ' 
+        Button13.BackColor = Color.LightGray
+        Button13.FlatAppearance.BorderColor = Color.LightGray
+        resources.ApplyResources(Button13, "Button13")
+        Button13.Name = "Button13"
+        Button13.UseVisualStyleBackColor = False
+        ' 
+        ' Button14
+        ' 
+        Button14.BackColor = Color.LightGray
+        Button14.FlatAppearance.BorderColor = Color.LightGray
+        resources.ApplyResources(Button14, "Button14")
+        Button14.Name = "Button14"
+        Button14.UseVisualStyleBackColor = False
+        ' 
+        ' Button15
+        ' 
+        Button15.BackColor = Color.LightGray
+        Button15.FlatAppearance.BorderColor = Color.LightGray
+        resources.ApplyResources(Button15, "Button15")
+        Button15.Name = "Button15"
+        Button15.UseVisualStyleBackColor = False
+        ' 
+        ' Button16
+        ' 
+        Button16.BackColor = Color.SandyBrown
+        Button16.FlatAppearance.BorderColor = Color.SandyBrown
+        resources.ApplyResources(Button16, "Button16")
+        Button16.Name = "Button16"
+        Button16.UseVisualStyleBackColor = False
+        ' 
+        ' Button17
+        ' 
+        Button17.BackColor = Color.SandyBrown
+        Button17.FlatAppearance.BorderColor = Color.SandyBrown
+        resources.ApplyResources(Button17, "Button17")
+        Button17.Name = "Button17"
+        Button17.UseVisualStyleBackColor = False
+        ' 
+        ' Button18
+        ' 
+        Button18.BackColor = Color.LightGray
+        Button18.FlatAppearance.BorderColor = Color.LightGray
+        resources.ApplyResources(Button18, "Button18")
+        Button18.Name = "Button18"
+        Button18.UseVisualStyleBackColor = False
+        ' 
+        ' Button19
+        ' 
+        Button19.BackColor = Color.SandyBrown
+        Button19.FlatAppearance.BorderColor = Color.SandyBrown
+        resources.ApplyResources(Button19, "Button19")
+        Button19.Name = "Button19"
+        Button19.UseVisualStyleBackColor = False
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.BackColor = Color.DarkGray
+        TextBox1.BorderStyle = BorderStyle.FixedSingle
+        resources.ApplyResources(TextBox1, "TextBox1")
+        TextBox1.Name = "TextBox1"
+        ' 
+        ' Label1
+        ' 
+        resources.ApplyResources(Label1, "Label1")
+        Label1.Name = "Label1"
+        ' 
+        ' Label2
+        ' 
+        resources.ApplyResources(Label2, "Label2")
+        Label2.Name = "Label2"
+        ' 
+        ' Form1
+        ' 
+        AllowDrop = True
+        resources.ApplyResources(Me, "$this")
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.Gray
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox1)
+        Controls.Add(Button19)
+        Controls.Add(Button18)
+        Controls.Add(Button17)
+        Controls.Add(Button16)
+        Controls.Add(Button15)
+        Controls.Add(Button14)
+        Controls.Add(Button13)
+        Controls.Add(Button12)
+        Controls.Add(Button11)
+        Controls.Add(Button10)
+        Controls.Add(Button9)
+        Controls.Add(Button8)
+        Controls.Add(Button7)
+        Controls.Add(Button6)
+        Controls.Add(Button5)
+        Controls.Add(Button4)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(exit_button)
+        FormBorderStyle = FormBorderStyle.None
+        Name = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents exit_button As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents Button18 As Button
+    Friend WithEvents Button19 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,100 @@
+﻿Public Class Form1
+    Private Sub exit_button_Click(sender As Object, e As EventArgs) Handles exit_button.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If TextBox1.Text = "0" Then TextBox1.Clear()
+        TextBox1.Text += "1"
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        TextBox1.Text += "2"
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        TextBox1.Text += "3"
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        TextBox1.Text += "4"
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        TextBox1.Text += "5"
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        TextBox1.Text += "6"
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        TextBox1.Text += "7"
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        TextBox1.Text += "8"
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        TextBox1.Text += "9"
+    End Sub
+
+    Private Sub Button18_Click(sender As Object, e As EventArgs) Handles Button18.Click
+        TextBox1.Text += "0"
+    End Sub
+    Private Sub Button19_Click(sender As Object, e As EventArgs) Handles Button19.Click
+        TextBox1.Text += ","
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        TextBox1.Clear()
+
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Label1.Text = TextBox1.Text
+        Label2.Text = "/"
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        Label1.Text = TextBox1.Text
+        Label2.Text = "*"
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        Label1.Text = TextBox1.Text
+        Label2.Text = "-"
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
+        Label1.Text = TextBox1.Text
+        Label2.Text = "+"
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If Label2.Text = "/" Then
+            TextBox1.Text = Val(Label1.Text) / Val(TextBox1.Text)
+            Label1.Text = ""
+            Label2.Text = "="
+        End If
+        If Label2.Text = "*" Then
+            TextBox1.Text = Val(Label1.Text) * Val(TextBox1.Text)
+            Label1.Text = ""
+            Label2.Text = "="
+        End If
+        If Label2.Text = "-" Then
+            TextBox1.Text = Val(Label1.Text) - Val(TextBox1.Text)
+            Label1.Text = ""
+            Label2.Text = "="
+        End If
+        If Label2.Text = "+" Then
+            TextBox1.Text = Val(Label1.Text) + Val(TextBox1.Text)
+            Label1.Text = ""
+            Label2.Text = "="
+        End If
+    End Sub
+End Class
